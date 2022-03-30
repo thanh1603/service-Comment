32ecb1e0cc7d66f810d4d2fda58cb6ac86f28ced
ThanhTran <transinhthanh1999@gmail.com>
2022-03-30 22:12:02 +0700

    thanh first commit

---
+++ src/main/java/com/example/serviceComment/ServiceCommentApplication.java
@@ -0,0 +1,13 @@
+package com.example.serviceComment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCommentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCommentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/serviceComment/ServiceCommentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serviceComment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceCommentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
